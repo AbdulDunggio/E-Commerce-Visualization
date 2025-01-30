2796b9c70e708ea9f8c333eccb6c2c11e3c7e9b4
AbdulDunggio <dunggioalil53@gmail.com>
2025-01-30 22:39:05 +0700

    Update query-for-kpicards.sql

--- query-for-kpicards.sql
+++ query-for-kpicards.sql
@@ -1,4 +1,4 @@
--- total revenue
+-- total revenue & orders
 select
 quarter(order_purchase_timestamp) as quarter,
 year(order_purchase_timestamp) as year,
@@ -8,3 +8,15 @@ oi.price + oi.shipping_charges as total_revenue
 from orders o
 join order_items oi
 on o.order_id = oi.order_id
+
+-- total unique subscriber
+select distinct
+c.customer_id,
+quarter(o.order_delivered_timestamp) as quarter,
+year(o.order_delivered_timestamp) as year,
+monthname(o.order_delivered_timestamp) as month,
+day(o.order_delivered_timestamp) as day
+from customers c
+join orders o
+on c.customer_id = o.customer_id
+where order_status = 'delivered'
