1db1aeaba5194da672135f32d1c0b9768597098a
AbdulDunggio <dunggioalil53@gmail.com>
2025-01-31 09:14:54 +0700

    Update query-for-kpicards.sql

--- query-for-kpicards.sql
+++ query-for-kpicards.sql
@@ -9,7 +9,7 @@ from orders o
 join order_items oi
 on o.order_id = oi.order_id
 
--- total unique subscriber
+-- total unique customer
 select distinct
 c.customer_id,
 quarter(o.order_delivered_timestamp) as quarter,
@@ -20,3 +20,25 @@ from customers c
 join orders o
 on c.customer_id = o.customer_id
 where order_status = 'delivered'
+
+-- ontime orders percentage
+select
+order_id,
+order_status,
+order_delivered_timestamp,
+order_estimated_delivery_date,
+CASE
+WHEN order_delivered_timestamp <= order_estimated_delivery_date
+then 'On-Time' ELSE 'Delay' end as deliv_p
+from orders
+where order_status = 'delivered'
+limit 10
+
+-- counting products sold
+select
+p.product_id,
+oi.order_id,
+count(*) over() as total
+from products p
+join order_items oi
+on oi.product_id = p.product_id
